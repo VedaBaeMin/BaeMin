aa6846918a59720fb9eda9eff54f5ed4c0333e66
YOUNGHO0 <31pqopq13@gmail.com>
2024-10-17 18:14:26 +0900

    Feat: Food.h 추가

--- Food.h
+++ Food.h
@@ -4,9 +4,16 @@
 
 #ifndef BAEMIN_FOOD_H
 #define BAEMIN_FOOD_H
-
+#include <string>
 
 class Food {
+
+    long foodId;
+    long orderId;
+    long foodCount;
+    long price;
+    std::string foodName;
+
 
 };
 
