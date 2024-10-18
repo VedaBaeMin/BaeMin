cf123d1c38d4ca466f18f21eb1d665ddc88f0a66
sim7582 <sim7582@gmail.com>
2024-10-18 15:57:53 +0900

    food 메소드 추가

--- Food.cpp
+++ Food.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "Food.h"
+
+Food::Food(long foodID, long orderID, long foodCount=0, long price=0) {
+    this->foodId=foodID;
+    this->orderId=orderID;
+    this->foodCount=foodCount;
+    this->price=price;
+};
+long Food::getFoodId() {
+    return this->foodId;
+}
