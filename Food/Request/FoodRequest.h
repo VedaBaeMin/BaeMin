9fbdaaec4e78b2ac55863000d07cea4e76d41415
YOUNGHO0 <31pqopq13@gmail.com>
2024-10-21 15:49:32 +0900

    Feat : 파일 요청 생성

---
+++ Food/Request/FoodRequest.h
@@ -0,0 +1,24 @@
+//
+// Created by lee on 2024. 10. 18..
+//
+
+#ifndef BAEMIN_FOODREQUEST_H
+#define BAEMIN_FOODREQUEST_H
+
+#include <string>
+#include "../Food.h"
+class FoodRequest {
+    long price;
+    std::string foodName;
+
+public:
+    FoodRequest(long price, std::string foodName):price(price), foodName(foodName){};
+
+     shared_ptr<Food> toFood(long foodIdx){
+         return make_shared<Food>(foodIdx,price,foodName);
+     }
+
+};
+
+
+#endif //BAEMIN_FOODREQUEST_H
