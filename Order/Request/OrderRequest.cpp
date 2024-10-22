2f898e3e2ae577dd11e13ed2a5f3d504cd487d56
YOUNGHO0 <31pqopq13@gmail.com>
2024-10-22 16:07:10 +0900

    Fix : OrderRequest.cpp 임시 주석처리

--- Order/Request/OrderRequest.cpp
+++ Order/Request/OrderRequest.cpp
@@ -4,14 +4,8 @@
 
 #include "OrderRequest.h"
 
-std::shared_ptr<Order> OrderRequest::toOrder(long orderId) {
-    return std::make_shared<Order>(orderId,sellerId,customerId,PENDING);
-}
-
-std::vector<std::shared_ptr<FoodWithOrder>> OrderRequest::toFoodWithOrder(long foodWithOrderIndex, long orderIdx) {
-    std::vector<std::shared_ptr<FoodWithOrder>> foodWithOrderVector;
-    for(auto i : foodList){
-        foodWithOrderVector.push_back(std::make_shared<FoodWithOrder>(foodWithOrderIndex,i.foodId,orderIdx,i.foodCount));
-    }
-    return foodWithOrderVector;
-}
+//std::shared_ptr<Order> OrderRequest::toOrder(long orderId) {
+//    return std::make_shared<Order>(orderId,sellerId,customerId,PENDING);
+//}
+//
+//
