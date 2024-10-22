33606af23d860e586446d1afbcb94a7428feec2d
sim7582 <sim7582@gmail.com>
2024-10-22 16:43:07 +0900

    CMakeLists.txt Qt 기본 세팅

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
+#include <QApplication>
+#include <QLabel>
 
-#include "Customer.h"
-#include "Seller.h"
-#include "UserService.h"
-int main() {
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
 
-    Customer * c = new Customer(1,"hello","112", "test",10123);
-//    Seller * s = new Seller(2,"seller", "test", "hello3", 10231);
-
-// 요청이 들어오면
-    UserService userService;
-    userService.createUser(c);
-
-
-
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-
-
-
-
+    QLabel hello("Hello " ,0);
+    hello.show();
+    return app.exec();
 }
