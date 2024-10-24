937ead24aa43af35caad5919cb77de2d51bc3f06
sim7582 <sim7582@gmail.com>
2024-10-24 10:50:57 +0900

    FoodRequest(QByteArray) 생성자 추가

--- Food/Request/FoodRequest.h
+++ Food/Request/FoodRequest.h
@@ -7,13 +7,30 @@
 
 #include <string>
 #include <memory>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include "../Food.h"
 #include "../FreshFood.h"
 #include "../FrozenFood.h"
+using namespace std;
 enum FoodType{
     FRESH,
-    FROZEN
+    FROZEN,
+    UNKNOWN
 };
+FoodType stringToFoodType(const string& type) {
+    static const std::unordered_map<std::string, FoodType> FoodTypeMap = {
+        {"FRESH", FRESH},
+        {"FROZEN", FROZEN}
+    };
+
+    auto it = FoodTypeMap.find(type);
+    if (it != FoodTypeMap.end()) {
+        return it->second;
+    } else {
+        return UNKNOWN; // 찾을 수 없는 경우
+    }
+}
 
 class FoodRequest {
     long price;
@@ -22,6 +39,13 @@ class FoodRequest {
 
 public:
     FoodRequest(long price, std::string foodName,FoodType type):price(price), foodName(foodName),type(type){};
+    FoodRequest(QByteArray byte){
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(byte);
+    QJsonObject jsonObj = jsonDoc.object();
+    this->price=jsonObj["price"].toInt();
+    this->foodName=jsonObj["foofName"].toString().toStdString();
+    this->type=stringToFoodType(jsonObj["foofName"].toString().toStdString());
+    }
 
      shared_ptr<Food> toFood(long foodIdx){
          if(type == FRESH){
