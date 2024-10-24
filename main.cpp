9ffed1132e284e7b68aa420464a8a9d9b83e3a03
YOUNGHO0 <31pqopq13@gmail.com>
2024-10-24 13:58:29 +0900

    Fix : QT 사용 가능하게 추가

--- main.cpp
+++ main.cpp
@@ -6,30 +6,18 @@
 #include "Food/Request/FoodRequest.h"
 #include "Food/Service/FoodService.h"
 #include "Order/Service/OrderService.h"
+#include "User/Type/Customer/NewbieUser.h"
+#include <QApplication>
+#include <QWidget>
 
-int main() {
+int main(int argc, char *argv[]) {
 
-    FoodRequest request(1000,"helloFood",FROZEN);
-    FoodService foodService;
+    QApplication a(argc, argv);
+    QWidget w;
+    w.show();
 
-    foodService.createFood(request);
-    const  shared_ptr<const Food> &ptr = foodService.readFood(1);
-    if(ptr != nullptr) std::cout <<  ptr->getFoodName() << std::endl;
-    cout << ptr->getFoodPrice() << endl;
-    UserService userService;
-    OrderService orderService(foodService,userService);
+    return a.exec();
 
-    OrderRequest orderRequest(1,2,1,2);
-    orderService.createOrder(orderRequest);
-
-    const shared_ptr<Order> &orderptr = orderService.readOrder(1);
-    cout << orderptr->gerOrderId() << endl;
-    cout << orderptr->getId() << endl;
-
-//    foodService.updateFood(ptr);
-
-//    const shared_ptr<Food> &ptr2 = foodService.readFood(1);
-//    if(ptr != nullptr) std::cout <<  ptr2->getFoodName() << std::endl;
     return 0;
 
 
